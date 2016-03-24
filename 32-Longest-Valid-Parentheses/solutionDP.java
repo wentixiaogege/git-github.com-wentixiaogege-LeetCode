16095b635d22a55ade745dcbf7cf26bb1448a36c
jack_88228 <wentixiaogege@163.com>
2016-03-23 22:48:25 -0700

    [Runtime Error]committed by LeetCode Extension

---
+++ 32-Longest-Valid-Parentheses/solutionDP.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int longestValidParentheses(String s) {
+        
+    if(s == null || s.length() ==0){
+        return 0;
+    }
+    // longest[i] 以i 结尾最大valid 长度
+    int[] longest = new int[s.length()];// if s.charAt(i) == '(' longest[i] =0 else
+                                        // if ==')' if s[i-1] == '(' longest[i] = longest[i-2]+2;
+                                        //          if s[i-1] == ')' 
+                                                                 // if  s[i-longest[i-1]-1] =='(' longest[i] = longest[i-1] +2 + longest[i-longest[i-1] -2]
+                                                                 // else  =0;? 
+    int max =0;
+    for(int i=0; i<s.length(); i++){
+        
+        if(i>=0 && s.charAt(i) == ')'){
+            if(s.charAt(i-1) == '('){
+                longest[i] = (i-2 >=0) ?longest[i-2]+2 : 2;
+            }else if(i-longest[i-1]-1 >= 0 && s.charAt(i-longest[i-1]-1) =='('){
+                longest[i] = longest[i-1]+2 +longest[i-longest[i-1]-2];
+            }   
+            max = Math.max(max,longest[i]);
+        }
+    }
+     return max;   
+    }
+}
